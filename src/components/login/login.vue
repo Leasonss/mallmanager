c62e1f2025dcd27c03e4cc6511db7611623e8e09
Leason <645160617@qq.com>
2019-09-30 11:44:48 +0800

    用户添加

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,63 +1,78 @@
 <template>
-<div class="login-wrap">
-<!-- 通过设置 label-position 属性可以改变表单域标签的位置，
-可选值为 top、left，当设为 top 时标签会置于表单域的顶部 -->
-<el-form 
-    class="login-form"
-    label-position="top" 
-    label-width="80px" 
-    :model="formData">
-  <h2>用户登录</h2>
-  <el-form-item label="用户名">
-    <el-input v-model="formData.username"></el-input>
-  </el-form-item>
-  <el-form-item label="密码">
-    <el-input type="password" v-model="formData.password"></el-input>
-  </el-form-item>
-  <el-button
-    @click.prevent="handleLogin()"
-   class="login-btn"  type="primary">登录</el-button>
-</el-form>
-</div>
-
+  <div class="login-wrap">
+    <!-- 通过设置 label-position 属性可以改变表单域标签的位置，
+    可选值为 top、left，当设为 top 时标签会置于表单域的顶部-->
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-button
+        @click.prevent="handleLogin()"
+        class="login-btn"
+        type="primary"
+        @click="openFullScreen"
+        v-loading.fullscreen.lock="fullscreenLoading"
+      >登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-   data(){
-       return{
-           formData:{
-               username:'',
-               password:''
-           }
-       }
-   },
-   methods:{
-    //  简化代码让代码看起来像是同步的
-     async handleLogin(){
-           const res= await this.$http.post('login',this.formData)
-                       
-                          // console.log(res);
-                          // this和then异步操作
-                          const {data,meta:{msg,status}
-                          }=res.data
-                          if(status===200){
-                            // 登录成功会有提示出现
-                            // 保存token 验证登录否则返回原来的登录页面
-                            localStorage.setItem('token', data.token)
-                            this.$router.push({name:'home'})
-                            this.$message.success(msg)
-                          }
-                          else{
-                            // 登录失败也会有提示根据状态码
-                            this.$message.warning(msg);
-                          }
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: ""
+      },
+      fullscreenLoading: false
+    };
+  },
+  methods: {
+    // openFullScreen() {
+    //   this.fullscreenLoading = true;
+    //   setTimeout(() => {
+    //     this.fullscreenLoading = false;
+    //   }, 4000);
+    // },
+    openFullScreen() {
+        const loading = this.$loading({
+          lock: true,
+          text: 'Loading',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        });
+        setTimeout(() => {
+          loading.close();
+        }, 1000);
+    },
+    //  简化  代码让代码看起来像是同步的
+    
+    
+    async handleLogin() {
+      const res = await this.$http.post("login", this.formData);
 
-     }
-
-
-
-
+      // console.log(res);
+      // this和then异步操作
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        // 登录成功会有提示出现
+        // 保存token 验证登录否则返回原来的登录页面
+        localStorage.setItem("token", data.token);
+        this.$router.push({ name: "home" });
+        this.$message.success(msg);
+      } else {
+        // 登录失败也会有提示根据状态码
+        this.$message.warning(msg);
+      }
+    }
 
     //  handleLogin(){
     //          this.$http.post('login',this.formData)
@@ -76,22 +91,22 @@ export default {
     //                       else{
     //                         // 登录失败也会有提示根据状态码
     //                         this.$message.warning(msg);
-                            
+
     //                       }
 
     //                    })
     //  }
-   }
-}
+  }
+};
 </script>
 
 <style>
-.login-wrap{
-    height:100%;
-    background-color: #324152;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .login-wrap .login-form {
   background-color: #fff;
@@ -99,7 +114,7 @@ export default {
   padding: 30px;
   border-radius: 5px;
 }
-.login-wrap .login-btn{
-    width:100%;
+.login-wrap .login-btn {
+  width: 100%;
 }
 </style>
--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -8,18 +8,23 @@
       <!-- 搜索 -->
       <el-row class="searchRow">
         <el-col>
-          <el-input placeholder="请输入内容" v-model="query" class="selectSearch">
+          <el-input
+            clearable
+            @clear="clearGetUser"
+            placeholder="请输入内容"
+            v-model="query"
+            class="selectSearch"
+          >
             <el-button @click="searchUser" slot="append" icon="el-icon-search"></el-button>
           </el-input>
-          <el-button type="success">添加用户</el-button>
+          <el-button type="success"  @click="showAddDialog">添加用户</el-button>
         </el-col>
       </el-row>
       <!-- 表格 -->
       <el-table :data="userList" style="width: 100%">
         <el-table-column type="index" label="#" width="60"></el-table-column>
         <el-table-column prop="username" label="姓名" width="80"></el-table-column>
-        <el-table-column prop="email" label="邮箱">
-        </el-table-column>
+        <el-table-column prop="email" label="邮箱"></el-table-column>
         <el-table-column prop="mobile" label="电话"></el-table-column>
         <el-table-column prop="create_time" label="创建时间">
           <template slot-scope="userList">{{userList.row.create_time|fmtdate}}</template>
@@ -30,12 +35,12 @@
           </template>
         </el-table-column>
         <el-table-column prop="role_name" label="操作">
-          <template slot-scope="scope">
-              <el-button plain size="mini" type="primary" icon="el-icon-edit" circle></el-button>
-              <el-button plain size="mini" type="danger" icon="el-icon-delete" circle></el-button>
-              <el-button plain size="mini" type="success" icon="el-icon-check" circle></el-button>
-            </template>
-          </el-table-column>
+          <template>
+            <el-button plain size="mini" type="primary" icon="el-icon-edit" circle></el-button>
+            <el-button plain size="mini" type="danger" icon="el-icon-delete" circle></el-button>
+            <el-button plain size="mini" type="success" icon="el-icon-check" circle></el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
     </el-breadcrumb>
@@ -46,8 +51,30 @@
       :page-sizes="[2, 4, 6, 8]"
       :page-size="2"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
+      :total="total"
+    ></el-pagination>
+
+    <!-- 编辑删除弹窗 -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+      <el-form :model="form">
+        <el-form-item   label="用户名" label-width="100px">
+          <el-input v-model="form.usernme"  autocomplete="off"></el-input>
+        </el-form-item>
+         <el-form-item  label="密码 " label-width="100px">
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+         <el-form-item  label="邮箱" label-width="100px">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+         <el-form-item  label="电话" label-width="100px">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 <script>
@@ -60,19 +87,42 @@ export default {
       //分页
       total: -1,
       pagenum: 1,
-      pagesize: 2
-    };
+      pagesize: 2,
+      dialogFormVisibleAdd:false,
+      form:{
+         usernme:'',
+         password:'',
+         email:'',
+         mobile:''
+      }
+    }
   },
   created() {
     this.getUserList();
   },
   methods: {
-    // 搜索用户
-      searchUser(){
-        this.getUserList()
+    // 添加用户发送请求
+     async addUser(){
+      this.dialogFormVisibleAdd=false
+      const res = await this.$http.post(`users`,this.form)
+      console.log(res)
+     
       },
+    // 点击之后显示添加对话框
+    showAddDialog(){
+      this.dialogFormVisibleAdd=true
+    },
 
-// 获取数据刷新数据
+    // 输入框清空后重新获取数据
+    clearGetUser() {
+      this.getUserList();
+    },
+    // 搜索用户
+    searchUser() {
+      this.getUserList();
+    },
+
+    // 获取数据刷新数据
     async getUserList() {
       const AUTH_TOKEN = localStorage.getItem("token");
       this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
@@ -87,22 +137,22 @@ export default {
         //   给表格,total赋值，获取数据成功提示框
         this.userList = users;
         this.total = total;
-        this.$message.success("获取数据成功！");
+        this.$message.success("刷新数据成功！");
       }
       console.log(res);
     },
     // 分页方法
     handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-        this.pagesize=val//赋值每页传进来多少条
-        this.pagenum=1//每次点击了页数规定多少条后回到首页开始排版
-        this.getUserList()//每页规定页数后重新获取排版
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
-        this.pagenum=val
-        this.getUserList()
-      }
+      console.log(`每页 ${val} 条`);
+      this.pagesize = val; //赋值每页传进来多少条
+      this.pagenum = 1; //每次点击了页数规定多少条后回到首页开始排版
+      this.getUserList(); //每页规定页数后重新获取排版
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.pagenum = val;
+      this.getUserList();
+    }
   }
 };
 </script>
